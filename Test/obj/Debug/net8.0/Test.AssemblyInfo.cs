85598609b834a1f42067aa0702cfbfa3a112f302
JudexGrim <om.jumaili01@gmail.com>
2024-09-21 22:00:46 +0300

    Added Table Views

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f7c596ae42b1a64152281b03843241ba7d3ac94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f7eef6b9760e85c871e28b5a5820d0bdbdc1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
